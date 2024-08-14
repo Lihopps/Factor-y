52e886a918b98ed2a55da90a6f9adc20b2ddd269
Lihopps <lihops.game@gmail.com>
2024-08-14 23:45:56 +0200

    passage en gui.relative

--- script/machine.lua
+++ script/machine.lua
@@ -67,14 +67,14 @@ local function allMachine(machines, contents)
     return true
 end
 
-local function allinput(inputs, inputchest,inputs_tank)
+local function allinput(inputs, inputchest, inputs_tank)
     local contentinput = inputchest.get_inventory(defines.inventory.chest).get_contents()
-    for _,tank in pairs(inputs_tank)do
-        for name,count in pairs(tank.get_fluid_contents())do
+    for _, tank in pairs(inputs_tank) do
+        for name, count in pairs(tank.get_fluid_contents()) do
             if contentinput[name] then
-                contentinput[name]=contentinput[name]+count
+                contentinput[name] = contentinput[name] + count
             else
-                contentinput[name]=count
+                contentinput[name] = count
             end
         end
     end
@@ -90,83 +90,64 @@ local function allinput(inputs, inputchest,inputs_tank)
     return true
 end
 
-local function remove_in_tank(name,obj,inputs_tank)
-    local count=math.ceil(obj.count)
-    local tot=0
-    while count >0  or tot<6 do
-        count=count-inputs_tank[tot].remove_fluid{name=name, amount=count}
-        tot=tot+1
+local function remove_in_tank(name, obj, inputs_tank)
+    local count = math.ceil(obj.count)
+    local tot = 0
+    while count > 0 or tot < 6 do
+        count = count - inputs_tank[tot].remove_fluid { name = name, amount = count }
+        tot = tot + 1
     end
 end
 
-local function removeall(inputs, inputchest,inputs_tank)
+local function removeall(inputs, inputchest, inputs_tank)
     for name, obj in pairs(inputs) do
-        if obj.type=="item" then
+        if obj.type == "item" then
             inputchest.get_inventory(defines.inventory.chest).remove({ name = name, count = math.ceil(obj.count) })
-        elseif type=="fluid" then
-            remove_in_tank(name,obj,inputs_tank)
+        elseif type == "fluid" then
+            remove_in_tank(name, obj, inputs_tank)
         end
     end
 end
 
-local function add_in_tank(name,obj,outputs_tank)
-    local count=math.floor(obj.count)
-    for _,tank in pairs(outputs_tank) do
-        count=count-tank.insert_fluid({name=name,amount=count})
-        if count<=0 then
+local function add_in_tank(name, obj, outputs_tank)
+    local count = math.floor(obj.count)
+    for _, tank in pairs(outputs_tank) do
+        count = count - tank.insert_fluid({ name = name, amount = count })
+        if count <= 0 then
             break
         end
     end
 end
 
-local function addin(outputs, outputchest,outputs_tank)
+local function addin(outputs, outputchest, outputs_tank)
     for name, obj in pairs(outputs) do
-        if obj.type=="item" then
+        if obj.type == "item" then
             outputchest.get_inventory(defines.inventory.chest).insert({ name = name, count = math.floor(obj.count) })
-        elseif type=="fluid" then
-            add_in_tank(name,obj,outputs_tank)
+        elseif type == "fluid" then
+            add_in_tank(name, obj, outputs_tank)
         end
     end
 end
 
-local function getNumberItem(name,obj, unit_number, type)
+local function getNumberItem(name, obj, unit_number, type)
     if type == "recipe" then
         local recipechest = global.machine[unit_number].recipechest
         local contentrecipe = recipechest.get_inventory(defines.inventory.chest).get_contents()
         return contentrecipe[name] or 0
     elseif type == "input" then
-        if obj.type=="item" then
+        if obj.type == "item" then
             local inputchest = global.machine[unit_number].inputchest
             local contentrecipe = inputchest.get_inventory(defines.inventory.chest).get_contents()
             return contentrecipe[name] or 0
-        elseif obj.type=="fluid" then
-            local contentrecipe=0
-            for _,entity in pairs(global.machine[unit_number].inputs_tank) do
-                contentrecipe=contentrecipe+entity.get_fluid_count(name)
+        elseif obj.type == "fluid" then
+            local contentrecipe = 0
+            for _, entity in pairs(global.machine[unit_number].inputs_tank) do
+                contentrecipe = contentrecipe + entity.get_fluid_count(name)
             end
             return contentrecipe
         end
     end
     return 0
-end
-
---- @param name string
---- @param sprite SpritePath
---- @param tooltip LocalisedString
---- @param handler GuiElemHandler
---- @return GuiElemDef
-local function frame_action_button(name, sprite, tooltip, handler)
-    return {
-        type = "sprite-button",
-        name = name,
-        style = "frame_action_button",
-        sprite = sprite .. "_white",
-        hovered_sprite = sprite .. "_black",
-        clicked_sprite = sprite .. "_black",
-        tooltip = tooltip,
-        mouse_button_filter = { "left" },
-        handler = { [defines.events.on_gui_click] = handler },
-    }
 end
 
 local function makeMachine(recipe, unit_number)
@@ -183,7 +164,7 @@ local function makeMachine(recipe, unit_number)
         }
     }
     for name, count in pairs(recipe.machines) do
-        local realNumber = getNumberItem(name,nil, unit_number, "recipe")
+        local realNumber = getNumberItem(name, nil, unit_number, "recipe")
         local color = { 0, 1, 0 }
         if realNumber < count then
             color = { 1, 0, 0 }
@@ -196,7 +177,8 @@ local function makeMachine(recipe, unit_number)
             {
                 type = "label",
                 style = "rcalc_machines_label",
-                caption = "[entity=" .. name .. "] x " .. format.number(realNumber, true, 2) .. " / " .. format.number(count, true, 2),
+                caption = "[entity=" ..
+                    name .. "] x " .. format.number(realNumber, true, 2) .. " / " .. format.number(count, true, 2),
                 style_mods = { font_color = color },
                 tooltip = "Total : " .. realNumber
 
@@ -226,7 +208,7 @@ local function makeRecipe(recipe, unit_number)
         }
     }
     for name, obj in pairs(recipe.inputs) do
-        local realNumber = getNumberItem(name,obj, unit_number, "input")
+        local realNumber = getNumberItem(name, obj, unit_number, "input")
         local color = { 0, 1, 0 }
         if realNumber < math.ceil(obj.count) then
             color = { 1, 0, 0 }
@@ -241,7 +223,10 @@ local function makeRecipe(recipe, unit_number)
                 style = "rcalc_machines_label",
                 style_mods = { font_color = color },
                 caption = "[" ..
-                obj.type .. "=" .. name .. "] x" .. format.number(realNumber, true, 2) .. " / " ..format.number(math.ceil(obj.count), true, 2) ,
+                    obj.type ..
+                    "=" ..
+                    name ..
+                    "] x" .. format.number(realNumber, true, 2) .. " / " .. format.number(math.ceil(obj.count), true, 2),
                 tooltip = "Total : " .. realNumber
 
             }
@@ -282,7 +267,7 @@ local function recipegui(unit_number)
         name = "recipegui",
         children = {}
     }
-    if recipe then
+    if next(recipe) ~= nil then
         flow.children[1] = makeMachine(recipe, unit_number)
         flow.children[2] = { type = "line", direction = "vertical" }
         flow.children[3] = makeRecipe(recipe, unit_number)
@@ -306,35 +291,33 @@ local function destroy_gui(player_index)
     window.destroy()
 end
 
---- @param e EventData.on_gui_click
-local function on_close_button_click(e)
-    local self = global.gui[e.player_index]
-    if not self then
-        return
-    end
-    self.elems.lihop_machine.visible = false
-    if game.players[e.player_index] then
-        if game.players[e.player_index].opened == self.elems.lihop_machine then
-            game.players[e.player_index].opened = nil
+--- @param e EventData.on_gui_closed
+local function on_gui_closed(e)
+    if e.entity then
+        if e.entity.name == "lihop-recipechest" then
+            local player = game.players[e.player_index]
+            if not player then return end
+            destroy_gui(e.player_index)
         end
     end
-    destroy_gui(e.player_index)
-end
-
---- @param e EventData.on_gui_click
-local function on_window_closed(e)
-    local self = global.gui[e.player_index]
-    self.elems.lihop_machine.visible = false
-    destroy_gui(e.player_index)
 end
 
 --- @param e EventData.on_gui_opened
 local function on_gui_opened(e)
     if e.entity then
-        if e.entity.name == "lihop-machine-electric-interface" then
+        if e.entity.name == "lihop-recipechest" then
             local player = game.players[e.player_index]
+            local elecinterface = e.entity.surface.find_entity("lihop-machine-electric-interface", e.entity.position)
+            if not elecinterface then return end
             if not player then return end
-            player.opened = e.entity
+            --player.opened = e.entity
+            create_gui(player, elecinterface.unit_number)
+        elseif e.entity.name == "lihop-machine-electric-interface" then
+            local player = game.players[e.player_index]
+            local recipechest = e.entity.surface.find_entity("lihop-recipechest", e.entity.position)
+            if not recipechest then return end
+            if not player then return end
+            player.opened=recipechest
             create_gui(player, e.entity.unit_number)
         end
     end
@@ -381,7 +364,7 @@ function machine.build(entity)
         inputchest = inputchest,
         outputchest = outputchest,
         inputs_tank = inputs_tank,
-        outputs_tank= outputs_tank,
+        outputs_tank = outputs_tank,
         recipe = {}
     }
 end
@@ -415,8 +398,8 @@ function machine.update(unit_number)
     local recipechest = global.machine[unit_number].recipechest
     local inputchest = global.machine[unit_number].inputchest
     local outputchest = global.machine[unit_number].outputchest
-    local inputs_tank=global.machine[unit_number].inputs_tank
-    local outputs_tank=global.machine[unit_number].outputs_tank
+    local inputs_tank = global.machine[unit_number].inputs_tank
+    local outputs_tank = global.machine[unit_number].outputs_tank
 
     if electric and recipechest and inputchest and outputchest then
         if electric.valid and recipechest.valid and inputchest.valid and outputchest.valid then
@@ -428,12 +411,16 @@ function machine.update(unit_number)
                     electric.electric_buffer_size = recipe.tags["energy"] / 60
                     electric.power_usage = recipe.tags["energy"]
                     if electric.energy == electric.electric_buffer_size and allMachine(recipe.tags["machines"], contentrecipe) then
-                        if allinput(recipe.tags["inputs"], inputchest,inputs_tank) then
-                            removeall(recipe.tags["inputs"], inputchest,inputs_tank)
-                            addin(recipe.tags["outputs"], outputchest,outputs_tank)
+                        if allinput(recipe.tags["inputs"], inputchest, inputs_tank) then
+                            removeall(recipe.tags["inputs"], inputchest, inputs_tank)
+                            addin(recipe.tags["outputs"], outputchest, outputs_tank)
                         end
                     end
                 end
+            else
+                    global.machine[unit_number].recipe = {}
+                    electric.electric_buffer_size = 100000 / 60
+                    electric.power_usage = 100000
             end
         end
     end
@@ -445,78 +432,78 @@ end
 --- @param player LuaPlayer
 --- @return GuiData
 function create_gui(player, unit_number)
-    local elems = gui.add(player.gui.screen,
-        {
-            type = "frame",
-            direction = "vertical",
-            name = "lihop_machine",
-            style_mods = { size = { 500, 300 } },
-            handler = { [defines.events.on_gui_closed] = on_window_closed },
-            {
-                type = "flow",
-                style = "flib_titlebar_flow",
-                drag_target = "lihop_machine",
-                {
-                    type = "label",
-                    style = "frame_title",
-                    caption = { "mod-name.RateCalculator" },
-                    ignored_by_interaction = true,
-                },
-                { type = "empty-widget", style = "flib_titlebar_drag_handle", ignored_by_interaction = true },
-                frame_action_button("close_button", "utility/close", { "gui.close-instruction" }, on_close_button_click),
-            },
+    if not player.gui.relative.lihop_machine then
+        local elems = gui.add(player.gui.relative,
             {
                 type = "frame",
-                style = "inside_shallow_frame",
                 direction = "vertical",
-                {
-                    type = "flow",
-                    name = "power",
-                    style_mods = { margin = 5, vertical_align = "center" },
-                    direction = "horizontal",
-                    {
-                        type = "label",
-                        style_mods = { left_padding = 3 },
-                        caption = "Power :"
-                    },
-                    {
-                        type = "progressbar",
-                        style_mods = { left_padding = 3 },
-                        value = 0.5
-                    },
-                    {
-                        type = "label",
-                        style_mods = { left_padding = 3 },
-                        caption = "Consumption :"
-                    },
-                    {
-                        type = "label",
-                        style_mods = { left_padding = 3 },
-                        caption = 0
-                    },
+                name = "lihop_machine",
+                style_mods = { size = { 500, 300 } },
+                anchor = {
+                    gui = defines.relative_gui_type.container_gui,
+                    position = defines.relative_gui_position.right,
+                    names = { "lihop-recipechest" }
                 },
                 {
-                    type = "scroll-pane",
-                    direction = "horizontal",
-                    name = "scrollRecipe",
-                    style_mods = { margin = 5 },
-                    recipegui(unit_number),
+                    type = "flow",
+                    style = "flib_titlebar_flow",
+                    {
+                        type = "label",
+                        style = "frame_title",
+                        caption = { "mod-name.RateCalculator" },
+                        ignored_by_interaction = true,
+                    },
+                    { type = "empty-widget", style = "flib_titlebar_drag_handle", ignored_by_interaction = true },
+                    },
+                {
+                    type = "frame",
+                    style = "inside_shallow_frame",
+                    direction = "vertical",
+                    {
+                        type = "flow",
+                        name = "power",
+                        style_mods = { margin = 5, vertical_align = "center" },
+                        direction = "horizontal",
+                        {
+                            type = "label",
+                            style_mods = { left_padding = 3 },
+                            caption = "Power :"
+                        },
+                        {
+                            type = "progressbar",
+                            style_mods = { left_padding = 3 },
+                            value = 0.5
+                        },
+                        {
+                            type = "label",
+                            style_mods = { left_padding = 3 },
+                            caption = "Consumption :"
+                        },
+                        {
+                            type = "label",
+                            style_mods = { left_padding = 3 },
+                            caption = 0
+                        },
+                    },
+                    {
+                        type = "scroll-pane",
+                        direction = "horizontal",
+                        name = "scrollRecipe",
+                        style_mods = { margin = 5 },
+                        recipegui(unit_number),
+                    }
                 }
-            }
-        })
-
-    player.opened = elems.lihop_machine
-    elems.lihop_machine.visible = true
-    elems.lihop_machine.bring_to_front()
-    elems.lihop_machine.force_auto_center()
-    --- @type GuiData
-    local self = {
-        elems = elems,
-        unit_number = unit_number
-    }
-    global.gui[player.index] = self
-    machine.update_gui(self)
-    return self
+            })
+        --- @type GuiData
+        local self = {
+            elems = elems,
+            unit_number = unit_number
+        }
+        global.gui[player.index] = self
+        machine.update_gui(self)
+        return self
+    end
+    return nil
 end
 
 function machine.update_gui(opened, bool)
@@ -533,12 +520,8 @@ end
 
 machine.events = {
     [defines.events.on_gui_opened] = on_gui_opened,
+    [defines.events.on_gui_closed] = on_gui_closed,
 }
-
-gui.add_handlers({
-    on_close_button_click = on_close_button_click,
-    on_window_closed = on_window_closed,
-})
 
 
 return machine
